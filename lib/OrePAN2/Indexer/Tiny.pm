7b09e419da8c6450b8fafc7b01d3ea4afbf29032
Takumi Akiyama <t.akiym@gmail.com>
2018-09-06 02:05:20 +0900

    Checking in changes prior to tagging of version 0.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 7077066..3ebf104 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension OrePAN2-Indexer-Tiny
    
     {{$NEXT}}
    
    +0.02 2018-09-05T17:05:00Z
    +
    +    - add missing dependency
    +
     0.01 2018-09-05T14:20:50Z
    
         - original version

--- lib/OrePAN2/Indexer/Tiny.pm
+++ lib/OrePAN2/Indexer/Tiny.pm
@@ -15,7 +15,7 @@ use IO::Uncompress::Gunzip ('$GunzipError');
 use IO::Zlib;
 use Parse::LocalDistribution;
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 sub new {
     my ($class, %args) = @_;
